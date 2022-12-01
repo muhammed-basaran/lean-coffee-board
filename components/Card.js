2624c90680912746c0dbe88c93fffe6a81cff13a
Muhammed Basaran <mbasaran0293@gmail.com>
2022-12-01 15:05:23 +0100

    add some style

---
+++ components/Card.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { FaTrash } from "react-icons/fa";
+import { nanoid } from "nanoid";
+import styled from "styled-components";
+
+export default function Card({ entries }) {
+  function handleDelete() {}
+
+  return (
+    <>
+      <section>
+        {entries.map((entry) => (
+          <StyledSection key={nanoid()}>
+            <StyledIcon onClick={handleDelete}>
+              <FaTrash />
+            </StyledIcon>
+            <h2>{entry.thoughts}</h2>
+            <p>{entry.name}</p>
+          </StyledSection>
+        ))}
+      </section>
+    </>
+  );
+}
+
+const StyledSection = styled.section`
+  background-color: brown;
+  color: white;
+  border: 2px solid black;
+  border-radius: 20px;
+  padding: 10px;
+  padding-left: 20px;
+  margin: 20px;
+  text-align: left;
+`;
+
+const StyledIcon = styled.div`
+  color: black;
+  border: 1px solid black;
+  width: 50px;
+  height: 30px;
+  background-color: beige;
+  cursor: pointer;
+  text-align: center;
+  padding-top: 5px;
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
+import Card from "../components/Card";
+import Header from "../components/Header";
+import Form from "../components/Form";
+import { useState } from "react";
+import styled from "styled-components";
+
 export default function HomePage() {
+  const [entries, setEntries] = useState([]);
+
+  const handleSubmit = (newEntry) => {
+    setEntries([...entries, { ...newEntry }]);
+  };
+
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <StyledBody>
+      <Header />
+      <div></div>
+      <Card entries={entries} />
+      <footer>
+        <Form onSubmit={handleSubmit} />
+      </footer>
+    </StyledBody>
   );
 }
+
+const StyledBody = styled.div`
+  text-align: center;
+  border: 2px solid black;
+  height: 100vh;
+  width: 100vw;
+  background-color: beige;
+`;
